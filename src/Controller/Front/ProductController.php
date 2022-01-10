05fcd30eb14a1470e1e3c205fee2d2cbe9e3a398
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-01-10 15:15:26 +0100

    front product show et list

--- src/Controller/Front/ProductController.php
+++ src/Controller/Front/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,4 +12,23 @@ class ProductController extends AbstractController
     // Faire la fonction qui affiche la liste des produits
     // Ainsi que celle qui affiche un produit en particulier.
 
+    /**
+     * @Route("products", name="product_list")
+     */
+    public function productList(ProductRepository $productRepository)
+    {
+        $products = $productRepository->findAll();
+
+        return $this->render("front/products.html.twig", ['products' => $products]);
+    }
+
+    /**
+     * @Route("product/{id}", name="product_show")
+     */
+    public function productShow($id, ProductRepository $productRepository)
+    {
+        $product = $productRepository->find($id);
+
+        return $this->render("front/product.html.twig", ['product' => $product]);
+    }
 }
