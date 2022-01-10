049608c0417e2202f9a7891602394479c40b3a43
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-01-10 14:44:52 +0100

    Admin Front

---
+++ src/Controller/Front/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+
+    // Faire la fonction qui affiche la liste des produits
+    // Ainsi que celle qui affiche un produit en particulier.
+
+}
