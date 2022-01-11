5f792b94142c0d808b0fa4295a1b1b1fc989708d
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-01-11 09:15:53 +0100

    AdminProductController

---
+++ src/Controller/Admin/AdminProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminProductController extends AbstractController
+{
+
+    // Pour les trois entités (Product, Category, Brand): faire le CRUD complet dans
+    // des AdminController
+
+    // Modèle des routes @Route("/admin/create/product/", name="admin_create_product") 
+}
