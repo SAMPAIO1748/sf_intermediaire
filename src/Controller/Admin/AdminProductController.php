fb8d74f1a0abe6fbd04c1098a0c417e692ccd520
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-01-11 09:17:19 +0100

    AdminProductController

--- src/Controller/Admin/AdminProductController.php
+++ src/Controller/Admin/AdminProductController.php
@@ -11,5 +11,5 @@ class AdminProductController extends AbstractController
     // Pour les trois entités (Product, Category, Brand): faire le CRUD complet dans
     // des AdminController
 
-    // Modèle des routes @Route("/admin/create/product/", name="admin_create_product") 
+    // Modèle des routes @Route("/admin/create/product/", name="admin_create_product")
 }
